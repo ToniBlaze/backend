537417f7716e97381b848685d49ca207316f0f86
Alessio <alessio.toninello@gmail.com>
2024-06-18 21:12:33 +0200

    create POST PUT DELET user endpoints

--- endpoints/Users_EndPoints.js
+++ endpoints/Users_EndPoints.js
@@ -20,6 +20,31 @@ router.get('/users/:id', async (req, res, next) => {
     }
 })
 
+router.post('/users', async (req, res, next) => {
 
+    res.status(201).json(
+        await (new userModel(req.body)).save()
+    )
+})
+
+router.put('/users/:id', async (req, res, next) => {
+    try {
+    res.status(200).json(
+        await userModel.findByIdAndUpdate(
+                        req.params.id, 
+                        req.body))
+    } catch (err) {
+        next();
+    }
+})
+
+router.delete('/users/:id', async (req, res, next) => {
+    try {
+    res.status(200).json(
+        await userModel.findByIdAndDelete(req.params.id))
+    } catch (err) {
+        next();
+    }
+})
 
 module.exports = router;
