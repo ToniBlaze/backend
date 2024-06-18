7fcdbfbb6b1fe9c1d623decf477aef3dc29f04b0
Alessio <alessio.toninello@gmail.com>
2024-06-18 21:14:26 +0200

    format

--- endpoints/Users_EndPoints.js
+++ endpoints/Users_EndPoints.js
@@ -1,50 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Models
-const userModel = require("../models/User")
+const userModel = require("../models/User");
 
-router.get('/users', async (req, res, next) => {
-    res.status(200).json(await userModel.find());
-})
+router.get("/users", async (req, res, next) => {
+  res.status(200).json(await userModel.find());
+});
 
-router.get('/users/:id', async (req, res, next) => {
-    try {
-        res.status(200).json(
-            await userModel.findById(
-                req.params.id
-            )
-        );
-    } catch (err) {
-        next();
-    }
-})
+router.get("/users/:id", async (req, res, next) => {
+  try {
+    res.status(200).json(await userModel.findById(req.params.id));
+  } catch (err) {
+    next();
+  }
+});
 
-router.post('/users', async (req, res, next) => {
+router.post("/users", async (req, res, next) => {
+  res.status(201).json(await new userModel(req.body).save());
+});
 
-    res.status(201).json(
-        await (new userModel(req.body)).save()
-    )
-})
+router.put("/users/:id", async (req, res, next) => {
+  try {
+    res
+      .status(200)
+      .json(await userModel.findByIdAndUpdate(req.params.id, req.body));
+  } catch (err) {
+    next();
+  }
+});
 
-router.put('/users/:id', async (req, res, next) => {
-    try {
-    res.status(200).json(
-        await userModel.findByIdAndUpdate(
-                        req.params.id, 
-                        req.body))
-    } catch (err) {
-        next();
-    }
-})
-
-router.delete('/users/:id', async (req, res, next) => {
-    try {
-    res.status(200).json(
-        await userModel.findByIdAndDelete(req.params.id))
-    } catch (err) {
-        next();
-    }
-})
+router.delete("/users/:id", async (req, res, next) => {
+  try {
+    res.status(200).json(await userModel.findByIdAndDelete(req.params.id));
+  } catch (err) {
+    next();
+  }
+});
 
 module.exports = router;
