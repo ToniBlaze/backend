2bb2464d8fe4ca5219110799a7d428269071a549
Alessio <alessio.toninello@gmail.com>
2024-06-25 21:24:13 +0200

    adding validator and bug fix

--- endpoints/Users_EndPoints.js
+++ endpoints/Users_EndPoints.js
@@ -1,36 +1,69 @@
 const express = require("express");
 const router = express.Router();
+const { validationResult } = require("express-validator");
 
 // Models
 const userModel = require("../models/User");
 
-router.get("/users", async (req, res, next) => {
-  res.status(200).json(await userModel.find());
-});
+// Middleware
+const AuthMiddleware = require("../middlewares/AuthMiddleware");
 
-router.get("/users/:id", async (req, res, next) => {
+const validation = require('../middlewares/Validation')
+const { validateUser } = validation
+
+
+router.get("/users", AuthMiddleware, async (req, res, next) => {
   try {
-    res.status(200).json(await userModel.findById(req.params.id));
+    const users = await userModel.find();
+    res.status(200).json(users);
   } catch (err) {
-    next();
+    next(err);
   }
 });
 
-router.put("/users/:id", async (req, res, next) => {
+router.get("/users/:id", AuthMiddleware, async (req, res, next) => {
   try {
-    res
-      .status(200)
-      .json(await userModel.findByIdAndUpdate(req.params.id, req.body));
+    const user = await userModel.findById(req.params.id);
+    if (!user) {
+      const error = new Error("Utente non trovato");
+      error.status = 404;
+      return next(error);
+    }
+    res.status(200).json(user);
   } catch (err) {
-    next();
+    next(err);
   }
 });
 
-router.delete("/users/:id", async (req, res, next) => {
+router.put("/users/:id", AuthMiddleware, validateUser, async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   try {
-    res.status(200).json(await userModel.findByIdAndDelete(req.params.id));
+    const updatedUser = await userModel.findByIdAndUpdate(req.params.id, req.body);
+    if (!updatedUser) {
+      const error = new Error("Utente non trovato");
+      error.status = 404;
+      return next(error);
+    }
+    res.status(200).json(updatedUser);
   } catch (err) {
-    next();
+    next(err);
+  }
+});
+
+router.delete("/users/:id", AuthMiddleware, async (req, res, next) => {
+  try {
+    const deletedUser = await userModel.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      const error = new Error("Utente non trovato");
+      error.status = 404;
+      return next(error);
+    }
+    res.status(200).json({ message: "Utente eliminato con successo" });
+  } catch (err) {
+    next(err);
   }
 });
 
--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,18 +1,39 @@
 const { body } = require('express-validator');
 
 const validateRegisterInput = [
-    body('name').notEmpty().trim().escape().isAlpha().withMessage('Il nome non può contenere numeri o simboli'),
-    body('lastname').notEmpty().trim().escape().isAlpha().withMessage('Il cognome non può contenere numeri o simboli'),
-    body('birthdate').notEmpty().trim().isDate().withMessage('Inserisci una data valida'),
-    body('zipCode').notEmpty().trim().escape().isNumeric().withMessage('Lo ZipCode puo\' avere solo numeri'),
-    body('city').notEmpty().trim().escape().isAlpha().withMessage('La città non può contenere numeri o simboli'),
-    body('email').notEmpty().trim().isEmail().withMessage('Email non valida'),
-    body('password').notEmpty().trim().isLength({ min: 6 }).withMessage('La password deve contenere almeno 6 caratteri'),
+    body('name').notEmpty().trim().withMessage("Nome mancante!")
+        .escape().isAlpha().withMessage('Il nome non puo\' contenere numeri o simboli'),
+
+    body('lastname').notEmpty().trim().withMessage("Cognome mancante!")
+        .escape().isAlpha().withMessage('Il cognome non puo\' contenere numeri o simboli'),
+
+    body('address.street').notEmpty().trim().withMessage('Indirizzo mancante'),
+
+    body('address.houseNumber').notEmpty().trim().withMessage('Numero civico mancante'),
+
+    body('address.zipCode').notEmpty().trim().withMessage("Valuta mancante!")
+        .escape().isNumeric().withMessage('Lo ZipCode puo\' avere solo numeri'),
+
+    body('address.city').notEmpty().trim().withMessage("Citta\' mancante!")
+        .escape().isAlpha().withMessage('La citta\' non puo\' contenere numeri o simboli'),
+
+    body('birthdate').notEmpty().trim().withMessage("Data di nascita mancante!")
+        .isISO8601().withMessage('Inserisci una data valida'),
+
+    body('email').notEmpty().trim().withMessage("Email mancante!")
+        .isEmail().withMessage('Email non valida'),
+
+    body('password').notEmpty().trim().withMessage("Password mancante!")
+        .isLength({ min: 6 }).withMessage('La password deve contenere almeno 6 caratteri')
+
 ];
   
 const validateLoginInput = [
-    body('email').notEmpty().trim().isEmail().withMessage('Email non valida'),
-    body('password').notEmpty().withMessage('Password mancante'),
+    body('email').notEmpty().trim().withMessage("Email mancante!")
+        .isEmail().withMessage('Email non valida'),
+
+    body('password').notEmpty().withMessage('Password mancante')
+
 ];
 
 const validateAccount = [
@@ -24,13 +45,31 @@ const validateAccount = [
         .escape().isNumeric().withMessage("Saldo deve essere un numero!"),
     body("currency")
         .notEmpty().trim().withMessage("Valuta mancante!")
-        .isString().withMessage("Valuta deve essere una stringa!"),
+        .isString().withMessage("Valuta non valida"),
     body("title")
         .notEmpty().trim().withMessage("Titolo mancante!")
-        .isString().withMessage("Titolo deve essere una stringa!"),
+        .isString().withMessage("Titolo non valido"),
 ];
 
 
-module.exports = { validateRegisterInput, validateLoginInput, validateAccount };
+const validateUser = [
+
+    body('address.street').notEmpty().trim().withMessage('Indirizzo mancante'),
+
+    body('address.houseNumber').notEmpty().trim().withMessage('Numero civico mancante'),
+
+    body('address.zipCode').notEmpty().trim().withMessage("Valuta mancante!")
+        .escape().isNumeric().withMessage('Lo ZipCode puo\' avere solo numeri'),
+
+    body('address.city').notEmpty().trim().withMessage("Citta\' mancante!")
+        .escape().isAlpha().withMessage('La citta\' non puo\' contenere numeri o simboli'),
+
+    body('email').notEmpty().trim().withMessage("Email mancante!")
+        .isEmail().withMessage('Email non valida'),
+
+];
+
+
+module.exports = { validateRegisterInput, validateLoginInput, validateAccount, validateUser };
   
 
